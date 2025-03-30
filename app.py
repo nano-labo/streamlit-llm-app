c822d18bc108039b34bd05a9863c768ebe104685
nano-labo <n.atiran@gmail.com>
2025-03-31 07:58:13 +0900

    first commit.

--- app.py
+++ app.py
@@ -4,46 +4,29 @@
 
 import streamlit as st
 
-st.title("サンプルアプリ②: 少し複雑なWebアプリ")
+st.title("専門家の相談チャンネル: ")
 
-st.write("##### 動作モード1: 文字数カウント")
-st.write("入力フォームにテキストを入力し、「実行」ボタンを押すことで文字数をカウントできます。")
-st.write("##### 動作モード2: BMI値の計算")
-st.write("身長と体重を入力することで、肥満度を表す体型指数のBMI値を算出できます。")
+st.write("##### 使い方")
+st.write("相談したい専門家を選択し、入力フォームに相談したい内容を入力し、「相談」ボタンを押すと相談結果が表示されます。")
 
 selected_item = st.radio(
-    "動作モードを選択してください。",
-    ["文字数カウント", "BMI値の計算"]
+    "相談したい専門家を選択してください。",
+    ["運動の専門家", "栄養の専門家"]
 )
 
 st.divider()
 
-if selected_item == "文字数カウント":
-    input_message = st.text_input(label="文字数のカウント対象となるテキストを入力してください。")
-    text_count = len(input_message)
+input_message = st.text_input(label="専門家に相談したい内容を入力してください。")
 
-else:
-    height = st.text_input(label="身長（cm）を入力してください。")
-    weight = st.text_input(label="体重（kg）を入力してください。")
-
-if st.button("実行"):
+if st.button("相談"):
     st.divider()
 
-    if selected_item == "文字数カウント":
-        if input_message:
-            st.write(f"文字数: **{text_count}**")
-
-        else:
-            st.error("カウント対象となるテキストを入力してから「実行」ボタンを押してください。")
-
-    else:
-        if height and weight:
-            try:
-                bmi = round(int(weight) / ((int(height)/100) ** 2), 1)
-                st.write(f"BMI値: {bmi}")
-
-            except ValueError as e:
-                st.error("身長と体重は数値で入力してください。")
-
-        else:
-            st.error("身長と体重をどちらも入力してください。")
+    from langchain_openai import ChatOpenAI
+    from langchain.schema import SystemMessage, HumanMessage
+    llm = ChatOpenAI(model_name="gpt-4o-mini", temperature=0)
+    messages = [
+        SystemMessage(content="あなたは{selected_item}です。"),
+        HumanMessage(content="{input_message}}"),
+    ]
+    result = llm(messages)
+    st.write(print(result.content))
